8e7a42c56bf944648fc8d7d011e7f8d2c6260b35
Emil Papelas <emilpapelas4@gmail.com>
2023-03-04 15:13:28 +0200

    22

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('{any}', function () {
     return view('app');
-})->where('any','.*');
-
-
-Route::get('{any}', function () {
-    return view('app');
-})->where('ny','.*');
+})->where('any','.*'); l
